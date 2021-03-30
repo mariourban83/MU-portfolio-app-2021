d77b0061dcadea3884a0e3df3452dd67a8d12ac6
mariourban83 <mario.urban.83@gmail.com>
2021-03-30 18:56:07 +0100

    Add some styles + Firebase deploy, deploy-OK

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // styles
@@ -9,8 +9,8 @@ import './index.scss'
 import App from './App'
 
 ReactDOM.render(
-	<HashRouter>
+	<BrowserRouter>
 		<App />
-	</HashRouter>,
+	</BrowserRouter>,
 	document.getElementById('root')
 )
--- src/views/TheContact.js
+++ src/views/TheContact.js
@@ -1,17 +1,48 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React from "react";
+import { Col } from "react-bootstrap";
 
 // styles
-import './TheContact.scss'
+import "./TheContact.scss";
 
 // component
 
 const TheContact = () => {
-	return (
-		<Container className="Contact">
-				<h2>Contact Page</h2>
-			</Container>
-	)
-  }
-  
-export default TheContact
+  return (
+    <Col className="col-12 col-sm-8 col-md-7 mx-auto">
+      <h1> Got a Question, Project in mind ?</h1>
+      <hr />
+
+      <form
+        className="gform pt-3"
+        method="POST"
+        action="https://script.google.com/macros/s/AKfycbzWVwEnsBg8m_fJ_LGTEF5YZTqABRbjxmZ_qTpu93VrqrVHsK4uuKs86AdFDQrS6j3m/exec"
+      >
+        <label>
+          Your Name
+          <br />
+          <input type="text" name="From" className="m-1" />
+        </label>
+        <br />
+        <label>
+          Your email
+          <br />
+          <input type="email" name="Email" className="m-1" />
+        </label>
+        <br />
+        <label>
+          Message
+          <br />
+          <input type="textarea" name=" Message" className="m-1" />
+        </label>
+        <br />
+        <input
+          type="submit"
+          value="Send Message"
+          className="rounded-pill blue-bg mt-3 px-3"
+        />
+      </form>
+    </Col>
+  );
+};
+
+export default TheContact;
