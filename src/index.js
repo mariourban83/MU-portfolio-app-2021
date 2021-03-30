7d913ffe0af13fd159b88ebf7ccdc8feed99271d
mariourban83 <mario.urban.83@gmail.com>
2021-03-30 13:08:59 +0100

    Change BrowserRouter for HashRouter for correct gh-pages rendering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // styles
@@ -9,8 +9,8 @@ import './index.scss'
 import App from './App'
 
 ReactDOM.render(
-	<BrowserRouter>
+	<HashRouter>
 		<App />
-	</BrowserRouter>,
+	</HashRouter>,
 	document.getElementById('root')
 )
