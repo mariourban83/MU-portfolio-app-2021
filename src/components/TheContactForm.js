37a84bb1515bec8cb8d4acd289463f616c83e1bf
mariourban83 <mario.urban.83@gmail.com>
2021-04-04 23:06:40 +0100

    Add ExpressJs & nodemailer for sending messages to  any mail account set in server.js file

---
+++ src/components/TheContactForm.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+function TheContactForm() {
+  const [status, setStatus] = useState("Send Message");
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Sending message...");
+    const { name, email, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+
+    let response = await fetch("http://192.168.8.163:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus("Send Message");
+
+    let result = await response.json();
+    alert(result.status);
+    // clearing the values
+    setEmail("");
+    setName("");
+    setMessage("");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit} className="text-center">
+      <div className="form-group">
+        <label htmlFor="name">Your Name</label>
+        <br />
+        <input
+          id="name"
+          type="text"
+          className="form-control"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">Your Contact Email</label>
+        <br />
+        <input
+          id="email"
+          type="email"
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="message" />
+        <br />
+        <textarea
+          id="message"
+          rows={2}
+          placeholder="Your message..."
+          className="form-control "
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </div>
+
+      <button type="submit" className="rounded-pill border-white blue-bg mt-3 px-3">
+        {status}
+      </button>
+    </Form>
+  );
+}
+
+export default TheContactForm;
--- src/views/TheContact.js
+++ src/views/TheContact.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col } from "react-bootstrap";
+import { Container, Col } from "react-bootstrap";
 
 // styles
 import "./TheContact.scss";
@@ -10,43 +10,13 @@ import TheContactForm from "../components/TheContactForm";
 
 const TheContact = () => {
   return (
-    <Col className="col-12 col-sm-8 col-md-7 mx-auto">
-      <h1> Got a Question, Project in mind ?</h1>
-      <hr />
-
-      <form
-        className="gform pt-3"
-        method="POST"
-        action="https://script.google.com/macros/s/AKfycbzWVwEnsBg8m_fJ_LGTEF5YZTqABRbjxmZ_qTpu93VrqrVHsK4uuKs86AdFDQrS6j3m/exec"
-      >
-        <label>
-          Your Name
-          <br />
-          <input type="text" name="From" className="m-1" />
-        </label>
-        <br />
-        <label>
-          Your email
-          <br />
-          <input type="email" name="Email" className="m-1" />
-        </label>
-        <br />
-        <label>
-          Message
-          <br />
-          <input type="textarea" name=" Message" className="m-1" />
-        </label>
-        <br />
-        <input
-          type="submit"
-          value="Send Message"
-          className="rounded-pill blue-bg mt-3 px-3"
-        />
-      </form>
-      <hr />
-      <br />
-      <TheContactForm />
-    </Col>
+    <Container fluid>
+      <Col className="col-md-8 mx-auto">
+        <h1> Got a Question, Project in mind ?</h1>
+        <hr />
+        <TheContactForm />
+      </Col>
+    </Container>
   );
 };
 
